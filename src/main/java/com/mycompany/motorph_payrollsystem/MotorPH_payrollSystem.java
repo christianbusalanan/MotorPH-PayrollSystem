42e044b12db7bc187ff199cac0301801b15e8bd4
chris <chris@LAPTOP-6BHOK9C1>
2025-02-11 22:58:15 +0800

    First Commit Payroll System Draft

--- src/main/java/com/mycompany/motorph_payrollsystem/MotorPH_payrollSystem.java
+++ src/main/java/com/mycompany/motorph_payrollsystem/MotorPH_payrollSystem.java
@@ -12,14 +12,14 @@ import java.time.LocalDate;
 public class MotorPH_payrollSystem {
 
     public static void main(String[] args) {
-        Employee christian = new Employee("69", "Busalanan",  "Christian" , LocalDate.of(2000,02,24), "homeless", "09293780115", "000", "000",  "000",  "000",   "Intern", "data analyst", "Data Science" ,"Jennifer", 4000, 4000, 4000, 4000, 4000, 4000,  "chanchan",  "awitawards" );
+        Employee christian = new Employee("69", "Busalanan",  "Christian" , LocalDate.of(2000,02,24), "Laguna", "09293780115", "000", "000",  "000",  "000",   "Intern", "data analyst", "Data Science" ,"Jennifer", 4000, 4000, 4000, 4000, 4000, 4000,  "chanchan",  "awitawards" );
         christian.viewEmployeeDetails();
        
        
-        HR cj= new HR("69", "Oraya",  "Christtelle" , LocalDate.of(2000,02,9), "homeless", "09293780115", "000", "000",  "000",  "000",   "Intern", "data analyst", "Human Resoruce" ,"Jennifer", 4000, 4000, 4000, 4000, 4000, 4000,  "cj",  "cj123" );
+        HR cj= new HR("69", "Oraya",  "Christtelle" , LocalDate.of(2000,02,9), "Laguna", "09293780115", "000", "000",  "000",  "000",   "Intern", "data analyst", "Human Resoruce" ,"Jennifer", 4000, 4000, 4000, 4000, 4000, 4000,  "cj",  "cj123" );
         cj.viewEmployeeDetails();
         
-        PayrollStaff joshua= new PayrollStaff("100", "Ricohermozo",  "Joshua" , LocalDate.of(2000,02,9), "homeless", "09293780115", "000", "000",  "000",  "000",   "Intern" ,"Jennifer", 4000, 4000, 4000, 4000, 4000, 4000,  "Josh",  "josh123");
+        PayrollStaff joshua= new PayrollStaff("100", "Ricohermozo",  "Joshua" , LocalDate.of(2000,02,9), "Manila", "09293780115", "000", "000",  "000",  "000",   "Intern" ,"Jennifer", 4000, 4000, 4000, 4000, 4000, 4000,  "Josh",  "josh123");
         joshua.viewEmployeeDetails();
         
     }
